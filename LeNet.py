81b3288f81c2bcf8474bb2d436a7ab654f494e4b
sheng15 <tspower@qq.com>
2018-10-18 16:12:47 +1100

    refactor vars

--- LeNet.py
+++ LeNet.py
@@ -11,32 +11,33 @@ import fetch_MNIST
 
 class LeNet(object):
     #The network is like:
-    #    conv1 -> pool1 -> conv2 -> pool2 -> fc1 -> relu -> fc2 -> relu -> softmax
+    #    firstConvLayer -> firstPoolingLayer -> secondConvLayer -> secondPoolingLayer -> firstFullConnectLayer -> relu -> secondFullConnectLayer -> relu -> softmax
     # l0      l1       l2       l3        l4     l5      l6     l7      l8        l9
-    def __init__(self, lr=0.1):
-        self.lr = lr
+    def __init__(self, learningRate=0.1):
+        self.learningRate = learningRate
         # 6 convolution kernal, each has 1 * 5 * 5 size
-        self.conv1 = xavier_init(6, 1, 5, 5)
+        self.firstConvLayer = xavier_init(6, 1, 5, 5)
         # the size for mean pool is 2 * 2, stride = 2
-        self.pool1 = [2, 2]
+        self.firstPoolingLayer = [2, 2]
         # 16 convolution kernal, each has 6 * 5 * 5 size
-        self.conv2 = xavier_init(16, 6, 5, 5)
+        self.secondConvLayer = xavier_init(16, 6, 5, 5)
         # the size for mean pool is 2 * 2, stride = 2
-        self.pool2 = [2, 2]
+        self.secondPoolingLayer = [2, 2]
         # fully connected layer 256 -> 200
-        self.fc1 = xavier_init(256, 200, fc=True)
+        self.firstFullConnectLayer = xavier_init(256, 200, fc=True)
         # fully connected layer 200 -> 10
-        self.fc2 = xavier_init(200, 10, fc=True)
+        self.secondFullConnectLayer = xavier_init(200, 10, fc=True)
 
     def forward_prop(self, input_data):
+    	##
         self.l0 = np.expand_dims(input_data, axis=1) / 255   # (batch_sz, 1, 28, 28)
-        self.l1 = self.convolution(self.l0, self.conv1)      # (batch_sz, 6, 24, 24)
-        self.l2 = self.mean_pool(self.l1, self.pool1)        # (batch_sz, 6, 12, 12)
-        self.l3 = self.convolution(self.l2, self.conv2)      # (batch_sz, 16, 8, 8)
-        self.l4 = self.mean_pool(self.l3, self.pool2)        # (batch_sz, 16, 4, 4)
-        self.l5 = self.fully_connect(self.l4, self.fc1)      # (batch_sz, 200)
+        self.l1 = self.convolution(self.l0, self.firstConvLayer)      # (batch_sz, 6, 24, 24)
+        self.l2 = self.mean_pool(self.l1, self.firstPoolingLayer)        # (batch_sz, 6, 12, 12)
+        self.l3 = self.convolution(self.l2, self.secondConvLayer)      # (batch_sz, 16, 8, 8)
+        self.l4 = self.mean_pool(self.l3, self.secondPoolingLayer)        # (batch_sz, 16, 4, 4)
+        self.l5 = self.fully_connect(self.l4, self.firstFullConnectLayer)      # (batch_sz, 200)
         self.l6 = self.relu(self.l5)                         # (batch_sz, 200)
-        self.l7 = self.fully_connect(self.l6, self.fc2)      # (batch_sz, 10)
+        self.l7 = self.fully_connect(self.l6, self.secondFullConnectLayer)      # (batch_sz, 10)
         self.l8 = self.relu(self.l7)                         # (batch_sz, 10)
         self.l9 = self.softmax(self.l8)                      # (batch_sz, 10)
         return self.l9
@@ -44,13 +45,13 @@ class LeNet(object):
     def backward_prop(self, softmax_output, output_label):
         l8_delta             = (output_label - softmax_output) / softmax_output.shape[0]
         l7_delta             = self.relu(self.l8, l8_delta, deriv=True)                     # (batch_sz, 10)
-        l6_delta, self.fc2   = self.fully_connect(self.l6, self.fc2, l7_delta, deriv=True)  # (batch_sz, 200)
+        l6_delta, self.secondFullConnectLayer   = self.fully_connect(self.l6, self.secondFullConnectLayer, l7_delta, deriv=True)  # (batch_sz, 200)
         l5_delta             = self.relu(self.l6, l6_delta, deriv=True)                     # (batch_sz, 200)
-        l4_delta, self.fc1   = self.fully_connect(self.l4, self.fc1, l5_delta, deriv=True)  # (batch_sz, 16, 4, 4)
-        l3_delta             = self.mean_pool(self.l3, self.pool2, l4_delta, deriv=True)    # (batch_sz, 16, 8, 8)
-        l2_delta, self.conv2 = self.convolution(self.l2, self.conv2, l3_delta, deriv=True)  # (batch_sz, 6, 12, 12)
-        l1_delta             = self.mean_pool(self.l1, self.pool1, l2_delta, deriv=True)    # (batch_sz, 6, 24, 24)
-        l0_delta, self.conv1 = self.convolution(self.l0, self.conv1, l1_delta, deriv=True)  # (batch_sz, 1, 28, 28)
+        l4_delta, self.firstFullConnectLayer   = self.fully_connect(self.l4, self.firstFullConnectLayer, l5_delta, deriv=True)  # (batch_sz, 16, 4, 4)
+        l3_delta             = self.mean_pool(self.l3, self.secondPoolingLayer, l4_delta, deriv=True)    # (batch_sz, 16, 8, 8)
+        l2_delta, self.secondConvLayer = self.convolution(self.l2, self.secondConvLayer, l3_delta, deriv=True)  # (batch_sz, 6, 12, 12)
+        l1_delta             = self.mean_pool(self.l1, self.firstPoolingLayer, l2_delta, deriv=True)    # (batch_sz, 6, 24, 24)
+        l0_delta, self.firstConvLayer = self.convolution(self.l0, self.firstConvLayer, l1_delta, deriv=True)  # (batch_sz, 1, 28, 28)
 
     def convolution(self, input_map, kernal, front_delta=None, deriv=False):
         N, C, W, H = input_map.shape
@@ -77,7 +78,7 @@ class LeNet(object):
                         kernal_gradient[kId][cId] += \
                           convolve2d(front_delta[imgId][kId], input_map[imgId,cId,::-1,::-1], mode='valid')
             # update weights
-            kernal += self.lr * kernal_gradient
+            kernal += self.learningRate * kernal_gradient
             return back_delta, kernal
 
     def mean_pool(self, input_map, pool, front_delta=None, deriv=False):
@@ -103,7 +104,7 @@ class LeNet(object):
             # front->back (propagate loss)
             back_delta = np.dot(front_delta, fc.T).reshape(input_data.shape)
             # update weights
-            fc += self.lr * np.dot(input_data.reshape(N, -1).T, front_delta)
+            fc += self.learningRate * np.dot(input_data.reshape(N, -1).T, front_delta)
             return back_delta, fc
 
     def relu(self, x, front_delta=None, deriv=False):
@@ -148,9 +149,9 @@ if __name__ == '__main__':
     # size of data;                  batch size
     data_size = train_imgs.shape[0]; batch_sz = 64;
     # learning rate; max iteration;    iter % mod (avoid index out of range)
-    lr = 0.01;     max_iter = 50000; iter_mod = int(data_size/batch_sz)
+    learningRate = 0.01;     max_iter = 50000; iter_mod = int(data_size/batch_sz)
     train_labs = convertToOneHot(train_labs)
-    my_CNN = LeNet(lr)
+    my_CNN = LeNet(learningRate)
     for iters in range(max_iter):
         # starting index and ending index for input data
         st_idx = (iters % iter_mod) * batch_sz
