8acc32f74275b824829179c29a71923257c93c30
sheng15 <tspower@qq.com>
2018-10-19 22:12:09 +1100

    patch2d and 3d

--- cnn.cpp
+++ cnn.cpp
@@ -5,7 +5,37 @@
 #include <memory>
 
 
-//convCalculate(float **inputArray,int *** kernelarray, )
+//convCalculate(float ***inputArray,int *** kernelarray, float *** outputArray, int stride, float bias ){
+
+float*** getPatch3D(float*** inputArray, int i, int j,int filterWidth, int filterHeight,int stride){
+    int start_i = i * stride;
+    int start_j = j * stride;
+    int sizeOfInput = sizeof(inputArray);
+    int depthOfInput = sizeof(inputArray[0]);
+    int heightOfInput = sizeof(inputArray[1]);
+    int widthOfInput = sizeof(inputArray[2]);
+
+    int*** outputArray;
+
+    for (int k = 0; k < depthOfInput; k++) {
+		for (int i = 0; i < filterHeight; i++) {
+			outputArray[k][i] = new float[filterWidth];
+		}
+	}
+
+    for (int k = 0; k < depthOfInput; k++) {
+		for (int i = start_i; i < start_i+filterHeight; i++) {
+			for (int j = start_j; j < start_j+filterWidth; j++){
+				outputArray[k][i-start_i][j-start_j] = inputArray[k][i][j]
+			}
+		}
+	}
+}
+
+
+
+
+
 
 float max(float a, float b) {
 	return (a < b) ? b : a;     // or: return comp(a,b)?b:a; for version (2)
@@ -54,9 +84,9 @@ public:
 		filterDepth = depth;
 
 		for (int i = 0; i < depth; i++) {
-			for (int j = 0; j < width; j++) {
-				weights[i][j] = new float[height];
-				weights_grad[i][j] = new float[height];
+			for (int j = 0; j < height; j++) {
+				weights[i][j] = new float[width];
+				weights_grad[i][j] = new float[width];
 			}
 		}
 		for (int i = 0; i < depth; i++) {
@@ -127,8 +157,8 @@ public:
 		convActivator = activator;
 		convLearningRate = learningRate;
 
-		int outputWidth = outputSize(inputWidth, filterWidth, zeroPadding, stride);
-		int outputHeight = outputSize(inputHeight, filterHeight, zeroPadding, stride);
+		convOutputWidth = outputSize(inputWidth, filterWidth, zeroPadding, stride);
+		convOutputHeight = outputSize(inputHeight, filterHeight, zeroPadding, stride);
 
 		for (int i = 0; i < filterNumber; i++) {
 			for (int j = 0; j < outputHeight; j++) {
@@ -150,8 +180,43 @@ public:
 			filters[i] = filter;
 		}
 		
-		void forward() {
-			
+		void forward(this,float *** input) {
+			inputArray = padding(input,this.zeroPadding);
+			for(int i = 0; i< this.convFilterNumber; i++){
+				Filter filter = this.filters[i];
+				for(int j = 0; j< this.convOutputHeight; j++){
+					for(int k = 0; k < this.convOutputWidth; k++){
+
+					}
+				}
+
+			}
+
+
+		def conv(input_array, 
+         kernel_array,
+         output_array, 
+         stride, bias):
+    '''
+    计算卷积，自动适配输入为2D和3D的情况
+    '''
+    channel_number = input_array.ndim
+    output_width = output_array.shape[1]
+    output_height = output_array.shape[0]
+    kernel_width = kernel_array.shape[-1]
+    kernel_height = kernel_array.shape[-2]
+    for i in range(output_height):
+        for j in range(output_width):
+            output_array[i][j] = (    
+                get_patch(input_array, i, j, kernel_width, 
+                    kernel_height, stride) * kernel_array
+                ).sum() + bias
+
+
+
+        conv(self.padded_input_array, 
+                filter.get_weights(), self.output_array[f],
+                self.stride, filter.get_bias())	
 
 
 
@@ -163,10 +228,10 @@ public:
 
 
 private:
-	int convInputWidth,convInputHeight,convChannelNumber,convFilterWidth,convFliterHeight,convFilterNumber,convZeroPadding,convStride;
+	int convInputWidth,convInputHeight,convChannelNumber,convFilterWidth,convFliterHeight,convFilterNumber,convZeroPadding,convStride,convOutputWidth,convOutputHeight;
 	Activator convActivator;
 	float convLearningRate;
-	float 
+	float *** inputArray;
 	float *** outputArray;
 	Filter filters[100];
 };
