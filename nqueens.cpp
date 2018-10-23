7f86e8e62ae66f63a125d5dfabbf80dfb9bece33
sheng15 <tspower@qq.com>
2018-10-23 20:54:19 +1100

    、

--- nqueens.cpp
+++ nqueens.cpp
@@ -142,20 +142,24 @@ int main(int argc, char  *argv[]){
     	while(salves){
     		MPI_Recv(&reply, 1, MPI_INT, MPI_ANY_SOURCE, REPLY, MPI_COMM_WORLD, &status);
     		slave = status.MPI_SOURCE;
+    		printf("receive notice from slave %d\n", slave );
 
     		if(reply == FINISHED){
     			MPI_Recv(&num_solutions, 1, MPI_INT, slave, NUM_SOLUTIONS, MPI_COMM_WORLD, &status);
-    			solutionCount += num_solutions;   			  		
+    			solutionCount += num_solutions;  
+    			printf("%d slave said it finished its work\n",slave ); 			  		
     		}
 
     		if (seeds){
     			MPI_Send(&newTask, 1, MPI_INT, slave, REQUEST, MPI_COMM_WORLD);
 
     			MPI_Send(&seeds, 1, MPI_INT, slave, SEED, MPI_COMM_WORLD);
+    			printf("send seed to salve %d\n", salve );
     			seeds --;
     		}else{
 
     			MPI_Send(&terminate, 1, MPI_INT, slave, REQUEST, MPI_COMM_WORLD);
+    			printf("message to terminate slave %d\n", slave );
     			salves --;
     		}
 
@@ -175,7 +179,7 @@ int main(int argc, char  *argv[]){
 
     		if(request == NEW_TASK){
     			MPI_Recv(&seed, 1, MPI_INT, slave, SEED, MPI_COMM_WORLD, &status);
-
+    			printf("%d receive seed message\n", slave);
     			int queens[N];
 
     			if(!collide(0,seed/(size*size),1,(seed/size)%size)&&!collide(0,seed/(size*size),2,seed%size)&&!collide(1,(seed/size)%size,2,seed%size)){
@@ -189,7 +193,7 @@ int main(int argc, char  *argv[]){
 
 
     			MPI_Send(&my_solutions, 1, MPI_INT, 0, NUM_SOLUTIONS, MPI_COMM_WORLD);
-    			printf("for seed %d ,slave %d find %d solutions.\n",seed,rank,my_solutions);
+    			//printf("for seed %d ,slave %d find %d solutions.\n",seed,rank,my_solutions);
 			}else{//receive terminate from master, stop then
 				done = true;
 			}
