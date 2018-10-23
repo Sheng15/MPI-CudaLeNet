14b7bee28c535c420d0e1cb84038e917b86ed855
sheng15 <tspower@qq.com>
2018-10-23 16:15:24 +1100

    fix out put

--- nqueens.cpp
+++ nqueens.cpp
@@ -187,7 +187,7 @@ int main(int argc, char  *argv[]){
 
 
     			MPI_Send(&my_solutions, 1, MPI_INT, 0, NUM_SOLUTIONS, MPI_COMM_WORLD);
-    			printf("for seed %d\n ,slave %d\n find %d \nsolutions",seed,rank,my_solutions);
+    			printf("for seed %d ,slave %d find %d solutions",seed,rank,my_solutions);
 			}else{
 				done = true;
 			}
@@ -196,7 +196,7 @@ int main(int argc, char  *argv[]){
 
     }
 
-
+    printf("num of solutions are %d\n",solutions);
     MPI_Finalize();
     return 0;
 }
