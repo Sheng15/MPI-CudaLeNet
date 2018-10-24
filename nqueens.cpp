6ca3d19a1297444d41249eadf9aa5149948d62ec
sheng15 <tspower@qq.com>
2018-10-24 18:39:10 +1100

    Update nqueens.cpp

--- nqueens.cpp
+++ nqueens.cpp
@@ -116,10 +116,11 @@ int main(int argc, char  *argv[]){
     if(rank == 0) {
     	MPI_Status masterStatus;
     	int slaves = MPIsize -1;
-    	while(true){
+    	int resultCount = 0;
+    	while(resultCount < slaves){
     		MPI_Recv(&reply, 1, MPI_INT, MPI_ANY_SOURCE, REPLY, MPI_COMM_WORLD, &masterStatus);
     		slave = masterStatus.MPI_SOURCE;
-    		printf("receive %d notice from slave %d\n", reply,slave );
+    		//printf("receive %d notice from slave %d\n", reply,slave );
 
     		if(reply == FINISHED || reply == READY){
 	    		if (seeds){
@@ -137,6 +138,7 @@ int main(int argc, char  *argv[]){
     			MPI_Recv(&slaveResult, 1, MPI_INT, slave, NUM_SOLUTIONS, MPI_COMM_WORLD, &masterStatus);
     			solutionCount +=slaveResult;
     			printf("from slave %d ,we receive %d solutions!\n",slave,slaveResult);
+    			resultCount++;
     		}
     	}
     }else{
