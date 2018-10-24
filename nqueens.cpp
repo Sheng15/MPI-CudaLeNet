bbc7d3fe1fe3daaee7be205e75573907adee84e2
sheng15 <tspower@qq.com>
2018-10-24 17:39:46 +1100

    Update nqueens.cpp

--- nqueens.cpp
+++ nqueens.cpp
@@ -170,7 +170,7 @@ int main(int argc, char  *argv[]){
     }
 
     MPI_Finalize();
-    printf("num of solutions are %d\n",solutionCount);
+    printf("num of solutions are %d\n",solutions);
     return 0;
 }
 
